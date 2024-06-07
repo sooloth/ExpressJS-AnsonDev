fab17a728550ab5c28bb6a8a30bf52c47d5c8a0f
sooloth <pangnuar@gmail.com>
2024-06-07 22:11:54 +0700

    complete Cookies

--- src/index.mjs
+++ src/index.mjs
@@ -1,18 +1,25 @@
 import express from 'express'
 import routes from './routes/index.mjs'
+import cookieParser from 'cookie-parser'
 
 
 const app = express();
 
 app.use(express.json())
+app.use(cookieParser("helloworld"))
 app.use(routes)
 
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/api/products", (req, res) => {
-    res.send([{id: 123, name: "chicken", price: 12.99}])
+app.get("/", (req, res) => {
+    res.cookie('hello','world', { maxAge: 30000,  signed: true})
+    res.status(201).send({ msg: "Hello"})
 })
+
+
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -3,7 +3,14 @@ import { Router } from "express";
 const router = Router();
 
 router.get('/api/products', (req, res) => {
-    res.send([{ id: 123, name: "chicken breast", price: 12.99}])
+    console.log(req.headers.cookie)
+    console.log(req.cookies)
+    console.log(req.signedCookies.hello)
+    if (req.signedCookies.hello && req.signedCookies.hello === 'world')
+        return res.send([{ id: 123, name: "chicken breast", price: 12.99}])
+    return res
+    .status(403)
+    .send({ msg: "sorry. you need the correct cookie"})
 })
 
 
